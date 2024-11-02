cd0932da4a4d8b0b1c95570c1541ca68570be944
MyriamSiad <myriam.siad.sio@gmail.com>
2024-11-02 19:48:04 +0100

    Création d'un Bot en Python
    Bot en python :
    - Permettant d'intégrer (veille informatique)  via un flux RSS dans le site

---
+++ Bot_Veille_Informatiqu.py
@@ -0,0 +1,39 @@
+import feedparser
+from flask import Flask, render_template
+import requests
+from bs4 import BeautifulSoup
+from docutils.nodes import title
+
+from numpy.f2py.crackfortran import entrypattern
+
+## Création d'un fichier XML Google Actualités sur Inoreader contenant le Flux de recherche Machine Learning
+## url : 'https://news.google.com/news/rss/search?q=Machine%20learning&hl=fr-FR&gl=FR&ceid=FR:fr'
+
+## Création d'une fonction qui recupère juste un nombre limités de Artciles, les plus récents
+
+app = Flask(__name__)
+
+def derniers_articles():
+    feed = feedparser.parse('https://www.microsoft.com/en-us/research/feed/')
+    articles = []
+
+    for entry in feed.entries[:5]:
+        title = entry.title
+        link = entry.link
+
+        summary_html = entry.summary
+        summary_text = BeautifulSoup(summary_html, "html.parser").get_text()  # Extrait le texte sans HTML
+        articles.append({"title": title, "summary": summary_text, "link": link})
+
+    return articles
+
+@app.route("/")
+def index():
+    articles = derniers_articles()
+    return render_template("veille_informatique.html", articles=articles)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
